73c1d22741d9782c3e3e6a62b343cacdefa4a4c1
Pragati Garud <pragati.garud.vc@startv.com>
2021-03-13 21:28:49 +0530

    Exported MultiSelect Component

--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
 
-import Example from "./components/Example";
+// import Example from "./components/Example";
 
 import "./index.css";
 import "antd/dist/antd.css";
 
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
 import MultiSelect from './components/MultiSelect';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Example />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <Example />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 export default MultiSelect;
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
